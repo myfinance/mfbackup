e5df05e09e3f6f053096b58ef3c7e5dcfb5d6b97
holgerfischer <holger@hfischer.org>
2022-03-25 15:48:50 +0000

    test
    Signed-off-by: holgerfischer <holger@hfischer.org>

--- mongo/restore.sh
+++ mongo/restore.sh
@@ -5,7 +5,7 @@ echo Starting-RESTORE
 echo ******************************************************
 
 
-mongorestore --uri=$MONGODB_URI  --username=$MONGODB_USER --password=$MONGODB_PASSWORD --authenticationDatabase=admin --gzip --archive=/mongodump/$FILE
+mongorestore --uri=$MONGODB_URI --username=$MONGODB_USER --password=$MONGODB_PASSWORD --authenticationDatabase=admin --gzip --archive=/mongodump/$FILE
 
 
 sleep 30 | echo End-BACKUP
