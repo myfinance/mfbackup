76aa79394ae9c2014b22c56d974372ebb4ab5155
holgerfischer <holger@hfischer.org>
2022-03-25 10:28:52 +0000

    mongo
    Signed-off-by: holgerfischer <holger@hfischer.org>

--- mongo/dump.sh
+++ mongo/dump.sh
@@ -7,6 +7,6 @@ NOW="$(date +"%F")-$(date +"%T")"
 
 FILE="$DB_NAME-$NOW"
 
-mongodump --uri=$MONGODB_URI  --out=/mongodump/db/$FILE
+mongodump --uri=$MONGODB_URI  --out=/mongodump/db/$FILE --username=root --password=vulkan --authenticationDatabase=admin
 
 sleep 30 | echo End-BACKUP
